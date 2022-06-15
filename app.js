a7e305d4411522c8f2fcf1d70db9752f64469eea
aeradcs <linalepova@mail.ru>
2022-06-15 22:46:51 +0700

    fix message in pngsNames

--- app.js
+++ app.js
@@ -1,77 +1,3 @@
-// listen to table row click to display images
-$(document).ready(function () {
-    $(document.body).on("click", "tr[data-href]", function () {
-        // get clicked path from table
-        let path = this.firstElementChild.textContent
-        console.log("JS: path " + path)
-
-        document.getElementById("imgSaved").src = ""
-        document.getElementById("imgSaved1").src = ""
-        getPngRequest("EO-Multitaper-PSD-gradiometers-," + path.replace(/\//g, ",").replace(/ /g, "-") + ".png", 'imgSaved')
-        getPngRequest("EC-Multitaper-PSD-gradiometers-," + path.replace(/\//g, ",").replace(/ /g, "-") + ".png", 'imgSaved1')
-    })
-})
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-//add cell to body row
-function addCell(tr, text) {
-    let tc = tr.insertCell();
-    let td = document.createTextNode(text);
-    tc.appendChild(td);
-}
-
-//add cell to head row
-function addColName(trHead, text) {
-    let th = document.createElement('th');
-    trHead.appendChild(th);
-    let data = document.createTextNode(text);
-    th.appendChild(data);
-}
-
-// fill table on nav-results window
-async function loadTab() {
-    let table = document.getElementById("tableResults");
-
-    // load path and dates of pngs from service
-    let response;
-    let paths = [];
-    let dates = [];
-    await makeRequest("http://84.237.87.18:7710/paths/", "GET", undefined).then((data) => {
-        response = data.replace(/'/g, "\"");
-        let obj = JSON.parse(response);
-        console.log("JS: response " + response);
-        if (response !== '{}') {
-            Object.entries(obj).forEach(([key, value]) => {
-                paths.push(key);
-                dates.push(value);
-            });
-
-            // clear table to avoid duplicated rows
-            $("#tableResults tr").remove();
-
-            // create tbody
-            paths.forEach((path, index) => {
-                let tr = table.insertRow();
-                tr.setAttribute('data-href', '')
-                addCell(tr, path)
-                addCell(tr, dates[index]);
-            });
-
-            // create thead
-            let tHead = table.createTHead();
-            let trHead = tHead.insertRow();
-            trHead.setAttribute('class', 'table-secondary')
-            addColName(trHead, "Путь к файлу");
-            addColName(trHead, "Дата обработки");
-        } else {
-            alert("Результатов еще нет.\nПерейдите на экран \"Рабочая среда\", выберите файл и поставьте задачу.\nПосле выполнения результаты появятся здесь.")
-        }
-    });
-}
-
 // fill select form on nav-env window
 window.onload = function () {
     let selector = document.getElementById("pathsSelector");
@@ -113,11 +39,12 @@ async function runTask() {
         await makeRequest("http://84.237.87.18:7710/jobs/0/", "GET", undefined).then((data) => {
             response = data;
             // change GUI to display job completing
-            if (response !== undefined && response.split("\n")[1].split("|")[0] !== "") {
+            if (response !== undefined
+                && response.split("\n")[1].split("|")[0] !== ""
+                && compareInputAndOutputFiles(response.split("\n")[1].split("|")[0], fname)) {
                 pngsNames.value = response.split("\n")[1].split("|")[0] + "|" + response.split("\n")[2].split("|")[0]
             } else {
                 pngsNames.value = "Для этого файла не получилось построить изображения PSD"
-
             }
         });
         console.log("JS: loadImgNames done");
@@ -134,39 +61,18 @@ async function runTask() {
     button.disabled = false
 }
 
-async function makeRequest(url, method, body) {
-    if (body === undefined) {
-        return await fetch(url, {
-            method: method,
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'text/plain'
-            },
-        }).then((response) => {
-            return response.json().then((data) => {
-                console.log("JS: makeRequest response \n" + data);
-                return data;
-            }).catch((err) => {
-                console.log("JS: makeRequest error \n" + err);
-            })
-        });
-    } else {
-        return await fetch(url, {
-            method: method,
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'text/plain'
-            },
-            body: body,
-        }).then((response) => {
-            return response.json().then((data) => {
-                console.log("JS: makeRequest response \n" + data);
-                return data;
-            }).catch((err) => {
-                console.log("JS: makeRequest error \n" + err);
-            })
-        });
-    }
+// compare .set and .png names -- if they are not equal, then task didn't complete successfully and images weren't built
+function compareInputAndOutputFiles(pngOutputFile, setInputFile) {
+    // retrieve .set path from .png name (images has name like "EO-Multitaper-PSD-gradiometers-," + path_to_set_file
+    // and all spaces and slashes replaced with dashes and commas)
+    pngOutputFile = pngOutputFile.replace(/EO-Multitaper-PSD-gradiometers-,/g, "")
+        .replace(/EC-Multitaper-PSD-gradiometers-,/g, "")
+        .replace(/-/g, " ")
+        .replace(/,/g, "/")
+        .replace(/.png/g, "");
+    console.log(pngOutputFile);
+    console.log(setInputFile);
+    return setInputFile === pngOutputFile;
 }
 
 // generate .py to specific task
@@ -321,6 +227,40 @@ async function generatePythonScript(fname) {
 
 }
 
+async function makeRequest(url, method, body) {
+    if (body === undefined) {
+        return await fetch(url, {
+            method: method,
+            headers: {
+
+                'Content-Type': 'application/json'
+            },
+        }).then((response) => {
+            return response.json().then((data) => {
+                console.log("JS: makeRequest response \n" + data);
+                return data;
+            }).catch((err) => {
+                console.log("JS: makeRequest error \n" + err);
+            })
+        });
+    } else {
+        return await fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: body,
+        }).then((response) => {
+            return response.json().then((data) => {
+                console.log("JS: makeRequest response \n" + data);
+                return data;
+            }).catch((err) => {
+                console.log("JS: makeRequest error \n" + err);
+            })
+        });
+    }
+}
+
 async function getPngRequest(pngName, htmlElemName) {
     fetch('http://84.237.87.18:7710/images/' + pngName)
         .then(res => {
@@ -335,14 +275,87 @@ async function getPngRequest(pngName, htmlElemName) {
 
 async function showPsdImgs() {
     let pngsNames = document.getElementById("pngsNames").value.split("|")
-    if (pngsNames[1] !== undefined){
+    if (pngsNames[1] !== undefined) {
         getPngRequest(pngsNames[0], 'imagePsd')
         getPngRequest(pngsNames[1], 'imagePsd1')
         console.log("JS: showPsdImgs");
     }
 }
 
-// 127.0.0.1:8000
-// 84.237.87.18:7710
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// fill table on nav-results window
+async function loadTab() {
+    let table = document.getElementById("tableResults");
+
+    // load path and dates of pngs from service
+    let response;
+    let paths = [];
+    let dates = [];
+    await makeRequest("http://84.237.87.18:7710/paths/", "GET", undefined).then((data) => {
+        response = data.replace(/'/g, "\"");
+        let obj = JSON.parse(response);
+        console.log("JS: response " + response);
+        if (response !== '{}') {
+            Object.entries(obj).forEach(([key, value]) => {
+                paths.push(key);
+                dates.push(value);
+            });
+
+            // clear table to avoid duplicated rows
+            $("#tableResults tr").remove();
+
+            // create tbody
+            paths.forEach((path, index) => {
+                let tr = table.insertRow();
+                tr.setAttribute('data-href', '')
+                addCell(tr, path)
+                addCell(tr, dates[index]);
+            });
+
+            // create thead
+            let tHead = table.createTHead();
+            let trHead = tHead.insertRow();
+            trHead.setAttribute('class', 'table-secondary')
+            addColName(trHead, "Путь к файлу");
+            addColName(trHead, "Дата обработки");
+        } else {
+            alert("Результатов еще нет.\nПерейдите на экран \"Рабочая среда\", выберите файл и поставьте задачу.\nПосле выполнения результаты появятся здесь.")
+        }
+    });
+}
+
+//add cell to body row
+function addCell(tr, text) {
+    let tc = tr.insertCell();
+    let td = document.createTextNode(text);
+    tc.appendChild(td);
+}
+
+//add cell to head row
+function addColName(trHead, text) {
+    let th = document.createElement('th');
+    trHead.appendChild(th);
+    let data = document.createTextNode(text);
+    th.appendChild(data);
+}
+
+// listen to table row click to display images
+$(document).ready(function () {
+    $(document.body).on("click", "tr[data-href]", function () {
+        // get clicked path from table
+        let path = this.firstElementChild.textContent
+        console.log("JS: path " + path)
+
+        document.getElementById("imgSaved").src = ""
+        document.getElementById("imgSaved1").src = ""
+        getPngRequest("EO-Multitaper-PSD-gradiometers-," + path.replace(/\//g, ",").replace(/ /g, "-") + ".png", 'imgSaved')
+        getPngRequest("EC-Multitaper-PSD-gradiometers-," + path.replace(/\//g, ",").replace(/ /g, "-") + ".png", 'imgSaved1')
+    })
+})
+
+
 
 
